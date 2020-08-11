94b439fe1c8baba05198f65ca9b773cfcf749861
MRManohar <lmmrider99@gmail.com>
2020-08-11 18:31:07 +0530

    FSJ0420A109

---
+++ photo-gallery/src/app/photo-frame/photo-frame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-frame',
+  templateUrl: './photo-frame.component.html',
+  styleUrls: ['./photo-frame.component.css']
+})
+export class PhotoFrameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
